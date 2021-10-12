edeadd8619d4aaf514f47513a79132ab00af4615
Kamran Sadin <mrsadin@gmail.com>
2021-10-12 18:40:51 +0330

    Addint new DTO

---
+++ src/PostsService/Dtos/PostPublishedDto.cs
@@ -0,0 +1,9 @@
+namespace PostsService.Dtos
+{
+    public class PostPublishedDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Event { get; set; }
+    }
+}
